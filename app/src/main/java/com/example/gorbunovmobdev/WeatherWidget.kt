0101f745eb424d5a2d6447e318ca00e63e620776
Denis <deniskkaa9@gmail.com>
2024-05-16 18:51:59 +0300

    Made weather widget (Glance)

--- app/src/main/java/com/example/gorbunovmobdev/WeatherWidget.kt
+++ app/src/main/java/com/example/gorbunovmobdev/WeatherWidget.kt
@@ -1,47 +1,33 @@
 package com.example.gorbunovmobdev
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.glance.ColorFilter
+import androidx.core.app.ActivityCompat
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.ImageProvider
-import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.actionRunCallback
-import androidx.glance.appwidget.background
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
@@ -52,21 +38,14 @@ import androidx.glance.layout.size
 import androidx.glance.layout.width
 import androidx.glance.text.Text
 import androidx.glance.unit.ColorProvider
-import coil.Coil
-import coil.compose.rememberImagePainter
-import coil.util.CoilUtils
-import com.bumptech.glide.Glide
 import com.example.gorbunovmobdev.ui.theme.brown
 import com.example.gorbunovmobdev.ui.theme.gold
 import com.example.gorbunovmobdev.ui.theme.jazzblue
 import com.example.gorbunovmobdev.ui.theme.jazzblue1
 import com.example.gorbunovmobdev.weatherRetrofit.LocationProvider
 import com.example.gorbunovmobdev.weatherRetrofit.WeatherResponse
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.URL
 import java.time.LocalTime
@@ -93,9 +72,13 @@ class WeatherWidget : GlanceAppWidget() {
                 val sharedPreferences = context.getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
                 val isLoggedIn = sharedPreferences.getBoolean("loggedIn", false)
 
-                when(isLoggedIn){
-                    true -> WeatherContent(context, weatherData)
-                    false -> weatherAuthorize()
+                if (checkLocationPermission(context)){
+                    weatherNotAvailable("Пожалуйста, предоставьте доступ к вашей геолокации в настройках")
+                } else {
+                    when (isLoggedIn) {
+                        true -> WeatherContent(context, weatherData)
+                        false -> weatherNotAvailable("Авторизуйтесь, чтобы пользоваться виджетом")
+                    }
                 }
             }
         }
@@ -261,7 +244,7 @@ fun WeatherContent(context: Context, weatherData: WeatherResponse?) {
 }
 
 @Composable
-fun weatherAuthorize() {
+fun weatherNotAvailable(text : String) {
 
     Column(
         modifier = GlanceModifier
@@ -299,7 +282,7 @@ fun weatherAuthorize() {
             ) {
 
             Text(
-                text = "Пожалуйста, авторизируйтесь чтобы использовать виджет",
+                text = text,
                 modifier = GlanceModifier,
                 style = androidx.glance.text.TextStyle(
                     fontSize = 14.sp,
@@ -310,6 +293,22 @@ fun weatherAuthorize() {
 
         }
     }
+}
+
+
+
+
+
+fun checkLocationPermission(context: Context) : Boolean{
+    // Если разрешения не получены
+    return ActivityCompat.checkSelfPermission(
+        context,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
 }
 
 
